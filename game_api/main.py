235469982958976eba80bb263a8f02dc24073ece
yurei79601 <yurei79601@gmail.com>
2024-10-17 16:41:18 +0800

    [feature] update questions and API

--- game_api/main.py
+++ game_api/main.py
@@ -1,4 +1,5 @@
 import random
+from typing import Dict
 
 from fastapi import FastAPI
 from questions import QUESTIONS_DICT
@@ -14,8 +15,23 @@ def read_root():
 
 # 定義一個接收 GET 請求的路由，並從 URL 中接收參數
 @app.get("/questions/")
-def read_item(question_type: str):
+def get_questions(question_type: str) -> Dict[str, str]:
     return {
         "question_type": question_type,
         "question": random.choice(QUESTIONS_DICT[question_type]),
+    }
+
+
+@app.get("/confirm_symbols/")
+def confirm_symbols(
+    A: str,
+    B: str,
+    C: str,
+    D: str,
+) -> Dict[str, bool]:
+    return {
+        "A": A == "行",
+        "B": B == "捨",
+        "C": C == "忍",
+        "D": D == "辦",
     }
